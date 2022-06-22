2d5282b5c29964ba90fb225000f0431d09c6bd3c
Ted <tugrulerdemdogru@gmail.com>
2022-06-23 00:29:12 +0300

    some errors

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,25 @@
 import React from 'react'
+import { useEffect } from 'react'
+import { useSelector, useDispatch } from 'react-redux'
+import { increment } from '../redux/counter/counterSlice'
 
 const Header = () => {
+  const value = useSelector((state) => state.counter.value)
+  const dispatch = useDispatch()
+
+  const handleD = () => {
+    dispatch(increment())
+  }
+
+  useEffect(() => {}, [dispatch, value])
+  
   return (
-    <div>Header</div>
+    <div>
+      {value}
+      <button onClick={handleD} className=" m-4 border-2 border-black p-2">
+        arttır
+      </button>
+    </div>
   )
 }
 
